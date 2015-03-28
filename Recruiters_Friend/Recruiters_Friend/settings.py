0477f9001ff56a368531af74bccc7d8969742754
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-03-29 01:24:53 +0530

    Add path of templates in settings.py

--- Recruiters_Friend/Recruiters_Friend/settings.py
+++ Recruiters_Friend/Recruiters_Friend/settings.py
@@ -86,3 +86,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
 os.path.join(BASE_DIR, 'static'),
 )
+
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),
+                    os.path.join(BASE_DIR, 'templates/common'),)
+
+
+
+)
