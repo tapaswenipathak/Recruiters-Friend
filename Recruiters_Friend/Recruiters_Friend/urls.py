7537257137463ac6504b1f4e3e04243f7c1dea09
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-03-29 01:16:19 +0530

    Added view for home page

--- Recruiters_Friend/Recruiters_Friend/urls.py
+++ Recruiters_Friend/Recruiters_Friend/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'Recruiters_Friend.views.home', name='home'),
+    url(r'^$', 'common.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
