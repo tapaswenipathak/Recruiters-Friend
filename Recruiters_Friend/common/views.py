2595258ba818ff569b23ad5aca04d6f003e2eda6
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-03-31 11:28:25 +0530

    view for about added

--- Recruiters_Friend/common/views.py
+++ Recruiters_Friend/common/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render_to_response
 # Create your views here.
 def home(request):
 	return render_to_response('index.html')
+
+def about(request)	:
+	return render_to_response('about.html')
