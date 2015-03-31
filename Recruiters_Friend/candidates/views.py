e2806a04352a2c95edf2d788524e3e19e197e1ab
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-04-01 02:53:20 +0530

    Showing API result to users.

--- Recruiters_Friend/candidates/views.py
+++ Recruiters_Friend/candidates/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 import sys
 import followers
@@ -11,8 +11,9 @@ def base1(request):
 def api_call(request):
 	data=request.GET.get('candidates')
 	print data
-	topic, names = followers.get_some_followers(data)
-	print topic, names 
-	response_data={'topic':topic, 'names':names}
-	return HttpResponse(json.dumps(response_data),content_type="application/json")
-
+	topic, users = followers.get_some_followers(data)
+	#print topic, users 
+	response_data={'topic':topic, 'names':users}
+	print('From views', json.dumps(response_data))
+	#return HttpResponse(json.dumps(response_data),content_type="application/json")
+	return render_to_response('candidates_result.html', response_data, context_instance=RequestContext(request))
