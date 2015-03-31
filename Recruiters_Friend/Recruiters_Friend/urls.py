bf2aa46e1781fc59a28998fd8b5ff55173d0498f
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-03-31 11:27:52 +0530

    about url mapped view entry corrected

--- Recruiters_Friend/Recruiters_Friend/urls.py
+++ Recruiters_Friend/Recruiters_Friend/urls.py
@@ -5,13 +5,11 @@ from jd.views import base, rating_calculation
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'common.views.home', name='home'),
-    url(r'^about/', common.views.about, name='about'),
+    url(r'^about/', 'common.views.about', name='about'),
     url(r'^jd/', base),
     url(r'^rating/', rating_calculation),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-
-    
 
 )
